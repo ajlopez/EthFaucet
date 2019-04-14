e8f5606417a15dbc2113a3a89d99e687ac775351
ajlopez <webmaster@ajlopez.com>
2019-04-14 19:24:07 -0300

    Deploy faucet with initial balance

--- migrations/2_deploy_faucet.js
+++ migrations/2_deploy_faucet.js
@@ -1,6 +1,6 @@
 var Faucet = artifacts.require("./Faucet.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Faucet, 1000000);
+  deployer.deploy(Faucet, 1000000, { value: 100000000000 });
 };
 
