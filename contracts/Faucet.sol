ff2668b5489dccddb79bb4abe4a9a205d3c4e985
ajlopez <webmaster@ajlopez.com>
2020-04-13 06:58:49 -0300

    Updated to use truffle@5.1.14

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -1,5 +1,4 @@
-
-pragma solidity >=0.4.21 <0.6.0;
+pragma solidity >=0.5.0 <0.6.0;
 
 contract Faucet {
     uint public amount;
--- migrations/2_deploy_faucet.js
+++
@@ -1,6 +0,0 @@
-var Faucet = artifacts.require("./Faucet.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Faucet, 1000000, { value: 100000000000 });
-};
-
