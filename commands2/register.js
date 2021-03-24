0f659978f2758188cdf693e6321c1bf8e55afc10
ajlopez <webmaster@ajlopez.com>
2021-03-24 12:03:09 -0300

    Improved register command

--- commands2/register.js
+++ commands2/register.js
@@ -4,33 +4,31 @@ module.exports = async function (context, args) {
     const configs = context.configs;
     const utils = context.utils;
     
-    try {
-        const username = args[0];
-        const password = args[1];
-        
-        console.log('check username availability');
-        const address0 = await context.command('call').execute([ 'root', 'registry', 'nameToAddress(string)', "'" + username + "'" , 'address' ]);
-        
-        if (address0)
-            throw "User already exists";
+    const username = args[0];
+    const password = args[1];
+    
+    console.log('check username availability');
+    const address0 = await context.command('call').execute([ 'root', 'registry', 'nameToAddress(string)', "'" + username + "'" , 'address' ]);
+    
+    if (address0)
+        throw "User already exists";
 
-        // TODO calculate prefix
-        const account = users.fromNameToAccount(username, password, 'rsk_testnet');
+    // TODO calculate prefix
+    const account = users.fromNameToAccount(username, password, 'rsk_testnet');
 
-        console.log('register username');
-        await context.command('invoke').execute([ 'registryowner', 'registry', 'register(string,address)', '"' +username + '",' + account.address ]);
-        console.log('address', account.address);
-        
-        const config = configs.loadConfiguration();
-        
-        config.accounts[username] = account;
-        
-        configs.saveConfiguration(config);
-        
-        return account;
-    }
-    catch (ex) {
-        console.log(ex);
-    }
+    console.log('register username');
+    await context.command('invoke').execute([ 'registryowner', 'registry', 'register(string,address)', '"' + username + '",' + account.address ]);
+    console.log('address', account.address);
+    console.log('funding user');
+    await context.command('invoke').execute([ 'faucetowner', 'faucet', 'transferToAddress(address)', username ]);
+    console.log('done');
+    
+    const config = configs.loadConfiguration();
+    
+    config.accounts[username] = account;
+    
+    configs.saveConfiguration(config);
+    
+    return account;
 }
 
